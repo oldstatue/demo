8bcdc5089b6ec3e1deba0138e4a5861ec393ec21
Alex <alexchambers000@gmail.com>
2020-06-09 23:01:19 +0100

    commit

--- script.sh
+++ script.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
-#TODO
-#move all sed cleanup to a single pass at the end
-
 #input file 
 i=input.html
 
 #output file 
-echo > log.xml
-o=log.xml
+echo > data.xml
+o=data.xml
 
 echo '<xml>' >> $o
 
 #iterate through the URLs in pages.txt 
 while read p; do
 
+	#use curl to download the html to the input file
 	curl $p > $i
 
 	echo '<poem>' >> $o
-
 	echo '<author>' >> $o
-	#awk '/By <a href="https:\/\/www.poetryfoundation.org\/poets\// {split($0,a,"<*a*>")} END {print a [2]}' $i | sed 's/<\/a//' >> $o
-	awk '/By <a href="https:\/\/www.poetryfoundation.org\/poets\// {print}' $i | sed 's/By <a[^>]*>//g' | sed 's/<\/a>//' | sed 's/\xc2\xa0//g' >> $o
+
+	#find the line with the author name...
+	#...remove the surround html...
+	#...and replace the angle brackets with escape characters before writing to the output file
+	awk '/By <a href="https:\/\/www.poetryfoundation.org\/poets\// {print}' $i | \
+		sed 's/By <a[^>]*>//g' | \
+		sed 's/<\/a>//' | sed 's/\xc2\xa0//g' >> $o
 
 	echo '</author>' >> $o
-
 	echo '<title>' >> $o
 
+	#find the html on the line before the poem title and print the next line
 	awk '/<h1 class="c-hdgSans c-hdgSans_2 c-mix-hdgSans_inline">/ {getline; print}' $i >> $o
 
 	echo '</title>' >> $o
 	echo '<text>' >> $o
 
+	#find the poem body and split each <div> into its own line...
+	#...remove the leftover html...
+	#...replace the angle brackets with escape characters...
+	#...and remove new line and carriage return special characters before writing to the output file
 	awk '/<div style="text-indent: -1em; padding-left: 1em;">/ {split($0,a,"</div>")} END {for (i in a) {print a [i]}}' $i | \
 		sed 's/<div style=\"text-indent: -1em; padding-left: 1em;\">//' | \
 		sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' | \
-		sed 's/\xc2\xa0//g' | \
-		sed 's/\r//g'  >> $o
+		sed 's/\xc2\xa0//g' | \sed 's/\r//g'  >> $o
 
 	echo '</text>' >> $o
 	echo '</poem>' >> $o
