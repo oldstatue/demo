62e1c9f8646833e835dd461ce8f45d0ce84cd79f
Alex <alexchambers000@gmail.com>
2020-06-10 22:13:57 +0100

    commit

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ The webpage is viewable [here](https://oldstatue.github.io/demo/).
 
 There are two main parts to this project, each handled by a separate file:
 
-1. The shell script, **`script.sh`**, first downloads the pages and converts the relevant information into an XML document using Linux command line tools. It takes as input a text file with a list of URLs, which I manually compiled. It uses `curl` to download the pages, `awk` to search for and isolate the relevant text on the page, and `sed` to correctly format the text. It then writes the output to an XML document called `data.xml`, in which each poem is represented as a record containing author, title and content information. 
-1. The webpage, **`index.html`**, then uses JavaScript to parse the XML document into a JavaScript object, sort the lists of authors and poems, and display the data as HTML. The page also allows you to search by poem title and author name, and the JavaScript dynamically updates the HTML to match the search criteria.
+1. The shell script, [**`script.sh`**](https://github.com/oldstatue/demo/blob/master/script.sh), first downloads the pages and converts the relevant information into an XML document using Linux command line tools. It takes as input a text file with a list of URLs, which I manually compiled. It uses `curl` to download the pages, `awk` to search for and isolate the relevant text on the page, and `sed` to correctly format the text. It then writes the output to an XML document called **`data.xml`**, in which each poem is represented as a record containing author, title and content information. 
+
+1. The webpage, [**`index.html`**](https://github.com/oldstatue/demo/blob/master/index.html), then uses JavaScript to parse the XML document into a JavaScript object, sort the lists of authors and poems, and display the data as HTML. The page also allows you to search by poem title and author name, and the JavaScript dynamically updates the HTML to match the search criteria.
 
 The individual files are also commented throughout with more detailed explanations of how they work.  
--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#input file 
-i=input.html
+#input file (given as an argument when running the script) 
+i=res/pages.txt
 
-#output file 
+#temporary html file to process
+echo > temp.html
+t=temp.html 
+
+#output file
 echo > res/data.xml
 o=res/data.xml
 
@@ -11,45 +15,41 @@ echo '<xml>' >> $o
 
 #iterate through the URLs in pages.txt 
 while read p; do
-
 	#use curl to download the html to the input file
-	curl $p > $i
+	curl $p > $t
 
 	echo '<poem>' >> $o
 	echo '<author>' >> $o
 
 	#find the line with the author name...
-	#...remove the surrounding html...
-	#...and replace the angle brackets with escape characters before writing to the output file
-	awk '/By <a href="https:\/\/www.poetryfoundation.org\/poets\// {print}' $i | \
-		sed 's/By <a[^>]*>//g' | \
-		sed 's/<\/a>//' | sed 's/\xc2\xa0//g' >> $o
-
+	#...and remove the surrounding html before writing to the output file
+	awk '/By <a href="https:\/\/www.poetryfoundation.org\/poets\// {print}' $t | \
+		sed 's/By <a[^>]*>//' | sed 's/<\/a>//' >> $o   
+	
 	echo '</author>' >> $o
 	echo '<title>' >> $o
 
 	#find the html on the line before the poem title and write the next line to the output file
-	awk '/<h1 class="c-hdgSans c-hdgSans_2 c-mix-hdgSans_inline">/ {getline; print}' $i >> $o
+	awk '/<h1 class="c-hdgSans c-hdgSans_2 c-mix-hdgSans_inline">/ {getline; print}' $t >> $o
 
 	echo '</title>' >> $o
 	echo '<text>' >> $o
 
 	#find the poem body and split each <div> into its own line...
 	#...remove the leftover html...
-	#...replace the angle brackets with escape characters...
-	#...and remove new line and carriage return special characters before writing to the output file
-	awk '/<div style="text-indent: -1em; padding-left: 1em;">/ {split($0,a,"</div>")} END {for (i in a) {print a [i]}}' $i | \
+	#...replace the angle brackets with escape characters before writing to the output file
+	awk '/<div style="text-indent: -1em; padding-left: 1em;">/ {split($0,a,"</div>")} END {for (i in a) {print a [i]}}' $t | \
 		sed 's/<div style=\"text-indent: -1em; padding-left: 1em;\">//' | \
-		sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' | \
-		sed 's/\xc2\xa0//g' | \sed 's/\r//g'  >> $o
+		sed 's/</\&lt;/g' | sed 's/>/\&gt;/g'   >> $o
 
 	echo '</text>' >> $o
 	echo '</poem>' >> $o
 
-done < res/pages.txt
+	sed -i 's/ *//' $o
+done < $i
 
 echo '</xml>' >> $o
 
 #remove the input file now it's no longer needed
-rm $i
+rm $t
 
